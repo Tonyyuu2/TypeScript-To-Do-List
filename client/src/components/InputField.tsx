8e29e6474002e6ecab627c28c26a3fccd63ab912
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-07-07 15:16:45 -0700

    add styling

--- client/src/components/InputField.tsx
+++ client/src/components/InputField.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from "react";
+import './styles.css'
 
 export const InputField = () => {
   return (
-    <div>InputField</div>
-  )
-}
+    <form className="input">
+      <input type="input" placeholder="Add a task..." className="input__box" />
+      <button className="input__submit" type="submit">GO</button>
+    </form>
+  );
+};
