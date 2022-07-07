eb3a35808c20f4d086ed6d20aba1593d55624357
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-07-07 15:33:38 -0700

    add styling

--- client/src/components/InputField.tsx
+++ client/src/components/InputField.tsx
@@ -5,7 +5,7 @@ export const InputField = () => {
   return (
     <form className="input">
       <input type="input" placeholder="Add a task..." className="input__box" />
-      <button className="input__submit" type="submit">GO</button>
+      <button className="input__button" type="submit">GO</button>
     </form>
   );
 };
