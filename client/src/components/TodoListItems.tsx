fe0f5777becdb4fb5d56fbdf20787f90dfbf8da1
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-07-07 17:32:06 -0700

    import icons

--- client/src/components/TodoListItems.tsx
+++ client/src/components/TodoListItems.tsx
@@ -1,14 +1,31 @@
 import React from "react";
-import './styles.css'
+import "./styles.css";
 import { Todo } from "../model";
+import { AiFillEdit, AiFillDelete, } from 'react-icons/ai'
+import { MdOutlineDone, } from 'react-icons/md'
 
 interface Props {
   key: number;
   todo: Todo;
-  todos: Todo[]
+  todos: Todo[];
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 
 export const TodoListItems = ({ todo, key, todos, setTodos }: Props) => {
-  return <li>{}</li>;
+  return (
+    <form className="todos__items">
+      <span className="todos__items--text">{todo.todo}</span>
+      <div>
+        <span className="icon">
+          <AiFillEdit />
+        </span>
+        <span className="icon">
+        <AiFillDelete />
+        </span>
+        <span className="icon">
+        < MdOutlineDone/>
+        </span>
+      </div>
+    </form>
+  );
 };
