e9c5c17557c3070507aa60c13a75a4d8e711484a
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-07-11 09:42:44 -0700

    finish project- with testing all edge cases

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,10 +4,13 @@ import { InputField } from "./components/InputField";
 import { TodoList } from "./components/TodoList";
 import { Todo } from "./model";
 
+// 
+
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
   const [todos, setTodos] = useState<Todo[]>([]);
   console.log("todos :", todos);
+
 
   const handleAdd = (e: React.FormEvent<EventTarget>) => {
     e.preventDefault();
@@ -21,7 +24,7 @@ const App: React.FC = () => {
     <div className="Body">
       <span className="heading">task.me</span>
       <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
-      <TodoList todos={todos} setTodos={setTodos}/>
+      <TodoList todos={todos} setTodos={setTodos} />
     </div>
   );
 };
--- client/src/model.ts
+++ client/src/model.ts
@@ -4,3 +4,21 @@ export interface Todo{
   isDone: boolean;
 }
 
+// type Actions =
+//   | { type: "add"; payload: string }
+//   | { type: "remove"; payload: number }
+//   | { type: "done"; payload: number };
+
+// const TodoReducer = (state: Todo[], action: Actions) => {
+//   switch (action.type) {
+//     case "add":
+//       return [
+//         ...state,
+//         { id: Date.now(), todo: action.payload, isDone: false },
+//       ];
+//     case "remove":
+//       return state.filter((todo) => todo.id !== action.payload);
+//     case 'done':
+//       return state.map((todo) => todo.id !== action.payloa d)
+//   }
+// };
